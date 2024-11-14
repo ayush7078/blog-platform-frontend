7c78a3902097b796710e2724539b5faa07d7562c
ayush7078 <ayushporwal7078@gmail.com>
2024-11-14 17:38:49 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -18,15 +18,16 @@ const App = () => {
 
 const AppContent = () => {
   const { user } = useSelector((state) => state.auth);
-
+  console.log("user", user);
+  
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <HeaderComponent />
       <Layout.Content style={{ padding: '20px' }}>
         <Routes>
           <Route path="/" element={<PostList />} />
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-          <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+          <Route path="/login" element={!user ||  user != null ? <Login /> : <Navigate to="/" />} />
+          <Route path="/register" element={!user || user != null ? <Register /> : <Navigate to="/" />} />
           <Route path="/edit-profile" element={user ? <EditProfile /> : <Navigate to="/login" />} />
         </Routes>
       </Layout.Content>
