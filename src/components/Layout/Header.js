8fed2f94d66d425c17129c0409c4b50093ea2593
ayush7078 <ayushporwal7078@gmail.com>
2024-11-15 04:01:43 +0530

    new changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Layout, Dropdown, Menu, Button, Space } from 'antd';
+import { Layout, Dropdown, Menu, Button, Space, Typography } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../../redux/actions/authActions';
 
 const { Header } = Layout;
+const { Text } = Typography;
 
 const HeaderComponent = () => {
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
- // const navigate = useNavigate();
 
   const menu = (
     <Menu>
@@ -40,6 +40,11 @@ const HeaderComponent = () => {
     <Header style={{ padding: '0 20px', background: '#fff', display: 'flex', justifyContent: 'space-between' }}>
       <div style={{ fontSize: '20px', fontWeight: 'bold' }}>Blogging Platform</div>
       <Space>
+        {user && (
+          <Text strong style={{ marginRight: '10px' }}>
+            {user.name}
+          </Text>
+        )}
         <Dropdown overlay={menu} trigger={['click']}>
           <Button icon={<UserOutlined />} />
         </Dropdown>
